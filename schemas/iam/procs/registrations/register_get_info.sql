a60564f8864227ea376de694633aba97391d0db1
ferd <beowulf1416@gmail.com>
2023-02-22 06:11:19 +0800

    add function to retrieve registration details

--- schemas/iam/procs/registrations/register_get_info.sql
+++ schemas/iam/procs/registrations/register_get_info.sql
@@ -1,12 +1,20 @@
-create procedure register_get_info(
+create function register_get_info(
     p_token iam.registrations.token%type
+)
+returns table (
+    id iam.registrations.id%type,
+    email iam.registrations.email%type,
+    created iam.registrations.created%type
 )
 language plpgsql
 as $$
 begin
+    return query
     select
-        email
-    from iam.registrations
+        a.id,
+        a.email,
+        a.created
+    from iam.registrations a
     where
         token = p_token
         and is_verified = false
