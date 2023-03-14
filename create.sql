a451ecd5d0bcee4345bb8499ee2f85b857c5e2f2
ferd <beowulf1416@gmail.com>
2023-03-14 18:30:58 +0800

    add test1 client and test1 user

--- create.sql
+++ create.sql
@@ -6,3 +6,4 @@ create extension if not exists pgcrypto;
 \ir schemas/create.sql
 \ir schemas/init.sql
 \ir schemas/init_test.sql
+\ir schemas/init_test1.sql
---
+++ schemas/init_test1.sql
@@ -0,0 +1,67 @@
+create procedure init()
+language plpgsql
+as $$
+declare
+    test_client_id uuid;
+    test_user_id uuid;
+begin
+    test_client_id := public.gen_random_uuid();
+    test_user_id := test_client_id;
+
+    call client.client_add(
+        test_client_id,
+        'test1',
+        'test1',
+        'test1 client'
+    );
+
+    call client.client_set_active(
+        test_client_id,
+        true
+    );
+
+    call iam.user_add(
+        test_user_id,
+        'test1@mailinator.com',
+        'tester1'
+    );
+
+    call iam.user_set_active(
+        test_user_id,
+        true
+    );
+
+    -- add people record
+    call people.people_add(
+        test_user_id,
+        'test1',
+        'test1',
+        'test1',
+        '',
+        '',
+        1::smallint, -- male
+        2::smallint,
+        2::smallint
+    );
+
+    call people.people_set_active(
+        test_user_id,
+        true
+    );
+
+    -- add contact email record
+    call people.contact_email_add(
+        test_user_id,
+        test_user_id,
+        'test1@mailinator.com'
+    );
+
+    call people.contact_email_set_active(
+        test_user_id,
+        true
+    );
+end
+$$;
+
+call init();
+drop procedure init();
