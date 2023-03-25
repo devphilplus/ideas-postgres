11c60b19fd65cdbc532d00a7ba8db31737650630
ferd <beowulf1416@gmail.com>
2023-03-26 07:01:59 +0800

    remove clients schema

--- schemas/create.sql
+++ schemas/create.sql
@@ -1,6 +1,5 @@
 -- tables
 \ir common/tables.sql
-\ir client/tables.sql
 \ir tenants/tables.sql
 \ir iam/tables.sql
 \ir accounting/tables.sql
@@ -10,7 +9,6 @@
 
 -- procs
 \ir common/procs.sql
-\ir client/procs.sql
 \ir tenants/procs.sql
 \ir iam/procs.sql
 \ir accounting/procs.sql
