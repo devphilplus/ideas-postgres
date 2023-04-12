d6a8714ff766e1a2587f277e3f114f10af1e9146
ferd <beowulf1416@gmail.com>
2023-04-12 21:56:54 +0800

    modify user_add proc to automatically add user to default tenant.

--- schemas/iam/procs/users/user_add.sql
+++ schemas/iam/procs/users/user_add.sql
@@ -5,6 +5,8 @@ create procedure user_add(
 )
 language plpgsql
 as $$
+declare
+    v_default_tenant_id tenants.tenants.id%type;
 begin
     insert into iam.users (
         id,
@@ -16,6 +18,28 @@ begin
         true,
         p_email,
         public.crypt(p_pw, public.gen_salt('md5'))
+    );
+
+    -- add user to default tenant
+    select
+        id into v_default_tenant_id
+    from tenants.tenant_default_fetch()
+    ;
+
+    call iam.user_tenant_add(
+        p_user_id,
+        v_default_tenant_id
+    );
+
+    call iam.user_tenant_set_active(
+        p_user_id,
+        v_default_tenant_id,
+        true
+    );
+
+    call iam.user_tenant_set_default(
+        p_user_id,
+        v_default_tenant_id
     );
 end
 $$;
