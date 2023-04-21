cb117ef8583dd44fb99fc867f7b784276cf57c9c
ferd <beowulf1416@gmail.com>
2023-04-21 23:23:56 +0800

    modify organization add proc

--- schemas/tenants/procs/organizations/organization_add.sql
+++ schemas/tenants/procs/organizations/organization_add.sql
@@ -1,5 +1,6 @@
 create procedure organization_add(
     p_tenant_id tenants.organizations.tenant_id%type,
+    p_organization_id tenants.organizations.id%type,
     p_name tenants.organizations.name%type,
     p_description tenants.organizations.description%type
 )
@@ -7,10 +8,12 @@ language plpgsql
 as $$
 begin
     insert into tenants.organizations (
+        id,
         tenant_id,
         name,
         description
     ) valeus (
+        p_organization_id,
         p_tenant_id,
         p_name,
         p_description
