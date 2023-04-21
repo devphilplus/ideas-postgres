0dee83f1469cf06c488a4cf58551d626037f1347
ferd <beowulf1416@gmail.com>
2023-04-22 05:38:44 +0800

    add account record

--- schemas/accounting/procs/accounts/account_add.sql
+++ schemas/accounting/procs/accounts/account_add.sql
@@ -1,6 +1,6 @@
 create procedure account_add(
     p_tenant_id accounting.accounts.tenant_id%type,
-    p_account_id accounting.accounts.account_id%type,
+    p_account_id accounting.accounts.id%type,
     p_name accounting.accounts.name%type,
     p_description accounting.accounts.description%type,
     p_currency_id accounting.accounts.currency_id%type
